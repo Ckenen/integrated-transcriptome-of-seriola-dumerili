db5f9d012f34f6fb80c4e278bcf44dac44a851ea
Zonggui Chen <chenzonggui@whu.edu.cn>
2024-03-28 11:58:38 +0800

    Completed common/goterm

---
+++ common/goterm/00_download.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# latest version
+# wget https://purl.obolibrary.org/obo/go/go-basic.obo
+
+# version in out study
+wget https://release.geneontology.org/2021-07-02/ontology/go-basic.obo
